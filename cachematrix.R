9dc0692cffe387836cf44d8754cd107c1879ca7d
FireFighter1017 <pascalbellerose@gmail.com>
2016-12-21 23:47:26 -0500

    My own version of cachematrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,72 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming aSsignment no. 2from R Programming Class at Coursera.org
+## Lexical Scoping
 
-## Write a short comment describing this function
+## The functions coded here are caching an inversed matrix
+## so it does not have to be computed again.
+## This is usefull when the data does not change
+## So we can use the functions to store or retrieve the inverse matrix
+
+##************************************************************
+##  -- MAKECACHEMATRIX --
+## -----------------------------------------------------------
+## The first function exposes a list of functions
+##    setsrc: Stores the source matrix using "set" function
+##    getsrc: Returns the source matrix using "get" function
+##    setinverse: inverse matrix and caches it using "setimx" function
+##    getinverse: Returns inverse matrix from cache using "getimx" function
 
 makeCacheMatrix <- function(x = matrix()) {
+  ## At first run, reset cached invers matrix
+    imxCached <- NULL
+  
+  ## -- SET --
+  ## Stores matrix  
+  setsrc <- function(y) {
+    x <<- y
+    imxCached <<- NULL
+  }
 
+  ## -- GET
+  ## Returns source matrix
+  getsrc <- function() x
+  
+  ## -- SETIMX
+  ## Stores in cache inverted matrix
+  setinv <- function(inverted) imxCached <<- inverted
+  
+  ## -- GETIMX
+  ## Retrieve inverted matrix from cache
+  getinv <- function() imxCached
+  
+  ## -- Exposed list of functions
+  list(setsrc=setsrc, getsrc=getsrc, setinv=setinv, getinv=getinv)
 }
 
-
-## Write a short comment describing this function
+##************************************************************
+##  -- CACHESOLVE --
+## -----------------------------------------------------------
+## The following function returns the inverse of the matrix. 
+## This function assumes that the matrix is always invertible.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+
+  imx <- x$getinv()
+  if(is.null(imx)) {
+    ## Retrieve source matrix
+    srcmx <- x$getsrc()
+    ## Inverse matrix
+    imx <- solve(srcmx)
+    ## send it to cache
+    x$setinv(imx)
+  }
+  else {
+  ## If inverted matrix already exists
+  ## inversion is skipped and return cached inv. matrix
+    message("getting cached data.")
+    imx <- x$getinv
+  }
+  
+  ## return inversed matrix
+  imx
 }
